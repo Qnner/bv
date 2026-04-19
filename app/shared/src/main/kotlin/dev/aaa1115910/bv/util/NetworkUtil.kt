56988f42e92c3d81358a624707ac6c326b6b02f9
Qnner <qnner@qq.com>
2026-04-19 23:22:30 +0800

    remove mainland lock

--- app/shared/src/main/kotlin/dev/aaa1115910/bv/util/NetworkUtil.kt
+++ app/shared/src/main/kotlin/dev/aaa1115910/bv/util/NetworkUtil.kt
@@ -32,29 +32,30 @@ object NetworkUtil {
     }
 
     suspend fun isMainlandChina() = withContext(Dispatchers.IO) {
-        val deferreds = locCheckUrls.map { locCheckUrl ->
-            async {
-                runCatching {
-                    val result = client.get(locCheckUrl).bodyAsText()
-                    logger.info { "Network result:\n$result" }
+        // val deferreds = locCheckUrls.map { locCheckUrl ->
+        //     async {
+        //         runCatching {
+        //             val result = client.get(locCheckUrl).bodyAsText()
+        //             logger.info { "Network result:\n$result" }
 
-                    val networkCheckResult = result
-                        .lines()
-                        .filter { it.isNotBlank() }
-                        .associate { with(it.split("=")) { this[0] to this[1] } }
+        //             val networkCheckResult = result
+        //                 .lines()
+        //                 .filter { it.isNotBlank() }
+        //                 .associate { with(it.split("=")) { this[0] to this[1] } }
 
-                    require(networkCheckResult["loc"] != "CN") { "BV doesn't support use in mainland China" }
-                    false
-                }.getOrDefault(true)
-            }
-        }
+        //             require(networkCheckResult["loc"] != "CN") { "BV doesn't support use in mainland China" }
+        false
+        //             false
+        //         }.getOrDefault(true)
+        //     }
+        // }
 
-        select {
-            deferreds.forEach { deferred ->
-                deferred.onAwait { it }
-            }
-        }.also {
-            deferreds.forEach { it.cancel() }
-        }
+        // select {
+        //     deferreds.forEach { deferred ->
+        //         deferred.onAwait { it }
+        //     }
+        // }.also {
+        //     deferreds.forEach { it.cancel() }
+        // }
     }
 }
